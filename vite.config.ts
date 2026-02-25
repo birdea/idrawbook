fc34223c289fa930ab16186052a3fa7b12e26554
birdea <birdea@gmail.com>
2026-02-25 17:02:23 +0900

    feat: complete sentry integration with source map support
    chore: restore .env.example

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
-export default defineConfig({
-    build: {
-        sourcemap: true, // 소스 맵 생성 활성화
-    },
-    plugins: [
-        sentryVitePlugin({
-            org: "o442414",
-            project: "javascript-react-idrawbook",
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    // .env 파일에서 환경 변수를 로딩합니다.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        build: {
+            sourcemap: true, // 소스 맵 생성 활성화
+        },
+        plugins: [
+            sentryVitePlugin({
+                org: env.SENTRY_ORG_SLUG,
+                project: env.SENTRY_PROJECT_SLUG,
+                authToken: env.SENTRY_AUTH_TOKEN,
+            }),
+        ],
+    };
 });
